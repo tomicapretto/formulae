485270f81a38f98cbd3f42687e200869eee4200b
tomicapretto <tomicapretto@users.noreply.github.com>
2024-09-22 21:49:21 +0000

    deploy: 6c807aa17362a9ce90c4aeab73307edcf1a1d860

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.1.dev232+g151db43',
+    VERSION: '0.1.dev233+g6c807aa',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
